8a2fc9ce5af1353513345726507f82355e7511f0
jaramill0 <hugo98jaramillo@gmail.com>
2021-03-23 21:21:10 -0600

    Proyecto de estado inicial funcionando

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' ,author: 'Hugo Jaramillo', appname: 'Web Development'});
+});
+/*Agregando Nueva Ruta*/
+router.get('/greeting',function(req, res, next){
+  res.send('Hola Jaramillo Web')
+})
+/*Ruta Creada */
+router.get('/direccion',function(req,res,next){
+  res.send('Programacion Web en Pila Completa II')
+})
+module.exports = router;
