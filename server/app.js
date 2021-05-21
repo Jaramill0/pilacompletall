4489bfe989ce7a9e6cee4d0e3b5c523df234cfec
jaramill0 <hugo98jaramillo@gmail.com>
2021-05-21 02:54:56 -0500

    importacion de Modulos Web Pack Dev Middlewares

--- server/app.js
+++ server/app.js
@@ -7,7 +7,42 @@ var logger = require('morgan');
 var indexRouter = require('@s-rutas/index');
 var usersRouter = require('@s-rutas/users');
 
+// Webpack Modules
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackDevConfig from '../webpack.dev.config';
+import webpackConfig from '../webpack.dev.config';
+
+// Consultar el modo en que se esta ejecutando la aplicacion
+const env = process.env.NODE_ENV || 'development';
+
+// se crea la aplicación express
 var app = express();
+// verificando el modo de ejecucion de la aplicacion
+if(env === 'development'){
+  console.log('Ejecutando en modo desarrollo: WebPack Hot Reloading');
+  //PASO 1.-agregando la ruta del   HMR
+  // reload=true : habilita la recarga del Front-End cuando hay cambios en el codigo fuente del Front-End
+  // Timeout=1000: Tiempo de espera entre recarga y recargar de la pagina
+  webpackConfig.entry = ['webpack-hot-middleware/client?reload=true&timeout=1000', webpackConfig.entry];
+
+  // PASO 2.- Agregamos el Plugin 
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  //PASO 3.- Crear el Compilador de WebPack
+  const compiler = webpack(webpackConfig);
+
+  //PASO 4.- Agregando el Middleware a la cadena de middlewares de la aplicacion
+  app.use(webpackDevMiddleware(compiler,{
+    publicPath: webpackDevConfig.output.publicPath
+  }));
+
+  //PASO 5.- Agregando el Webpack Hot Middleware
+  app.use(webpackHotMiddleware(compiler));
+}else{
+  console.log("Ejecutando en modo Produccion...");
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
