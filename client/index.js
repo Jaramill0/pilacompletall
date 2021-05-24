8927d9e6df9ba7dcde0418d1cd0cef3592cf3291
jaramill0 <hugo98jaramillo@gmail.com>
2021-05-24 16:25:15 -0500

    Sesion 26 :  correccion de Errores en Index con Eslint.

--- client/index.js
+++ client/index.js
@@ -1,23 +1,27 @@
-import './stylesheets/style.css'
-import './stylesheets/mystylesheet.css'
+import './stylesheets/style.css';
+import './stylesheets/mystylesheet.css';
 
-console.log("Web Pack Trabajando !! ");
+// eslint-disable-next-line no-console
+console.log('Web Pack Trabajando !! ');
 // Default parameters estan disponible ES6/2015
-let show = (m = "Hot Module Replacement Working") => {
-    alert(m)
+const show = (m = 'Hot Module Replacement Working') => {
+  // eslint-disable-next-line no-alert
+  alert(m);
 };
 show();
 
 function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('resolved')
-        }, 2000);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
 }
-async function asyncCall(){
-    console.log("Calling an async function");
-    const result = await resolveAfter2Seconds();
-    console.log(result);
+async function asyncCall() {
+  // eslint-disable-next-line no-console
+  console.log('Calling an async function');
+  const result = await resolveAfter2Seconds();
+  // eslint-disable-next-line no-console
+  console.log(result);
 }
 asyncCall();
