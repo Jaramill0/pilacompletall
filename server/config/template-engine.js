85bf60a5adfe3e7255ff7e4f56ef6e01dc86452b
jaramill0 <hugo98jaramillo@gmail.com>
2021-06-02 00:52:02 -0500

    Sesion 28 Creacion de carpeta config en server agregando nuevo archivo template-engine para exportar el paquete express handlebars

---
+++ server/config/template-engine.js
@@ -0,0 +1,13 @@
+import exphbs from 'express-handlebars';
+import path from 'path';
+
+//primero  se exporta una funcion de configuracion
+export default (app) => {
+  // Registrar el motor de plantillas
+  app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main' }));
+  //2. Selecciona el motor de plantillas Recien Registro
+  app.set('view engine', 'hbs');
+  //3. Establecer la ruta de las vistas
+  app.set('views', path.join(__dirname, '..', 'views'));
+  return app;
+};
