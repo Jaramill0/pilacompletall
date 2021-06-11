e48adadd155a83f350bb7deb36c5215808d0a26d
jaramill0 <hugo98jaramillo@gmail.com>
2021-06-10 22:25:15 -0500

    Sesion 30 Loggeando con Winston Estatus de Error

--- server/app.js
+++ server/app.js
@@ -73,6 +73,10 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
+  // Log
+  winston.error(
+    `Code 404 Message: Page Not Found, ${req.originalUrl}, Method: ${req.method},`,
+  );
   next(createError(404));
 });
 
@@ -81,6 +85,10 @@ app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // Loggeando con Winston
+  winston.error(
+    `status: ${err.status || 500} ${err.message} ${req.method} IP:${req.ip}`,
+  );
 
   // render the error page
   res.status(err.status || 500);
