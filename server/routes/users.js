c0479daf54a2323f76916843ed132afe4330fd21
jaramill0 <hugo98jaramillo@gmail.com>
2021-06-11 20:05:56 -0500

    Sesion 31 Importando el Controlador enlazando la instancia de Router en Archivo Users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+import { Router } from 'express';
+// Importando el controlador
+import userController from '../controllers/userController';
+// Crenado instancia  de Router
+const router = new Router();
+// GET users listing
+router.get('/', userController.index);
+export default router;
