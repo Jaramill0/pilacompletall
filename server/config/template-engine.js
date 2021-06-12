45991bee114acaebd8161466f5e9eafaa1ad3e0a
jaramill0 <hugo98jaramillo@gmail.com>
2021-06-11 20:01:41 -0500

    Sesion 31 coreccion de error de Eslint en espacios

--- server/config/template-engine.js
+++ server/config/template-engine.js
@@ -1,13 +1,12 @@
 import exphbs from 'express-handlebars';
 import path from 'path';
-
-//primero  se exporta una funcion de configuracion
+// primero  se exporta una funcion de configuracion
 export default (app) => {
   // Registrar el motor de plantillas
   app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main' }));
-  //2. Selecciona el motor de plantillas Recien Registro
+  // 2. Selecciona el motor de plantillas Recien Registro
   app.set('view engine', 'hbs');
-  //3. Establecer la ruta de las vistas
+  // 3. Establecer la ruta de las vistas
   app.set('views', path.join(__dirname, '..', 'views'));
   return app;
 };
