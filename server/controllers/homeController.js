63dfd39f05f60172595413937049d44bd2d2c1fe
jaramill0 <hugo98jaramillo@gmail.com>
2021-06-24 01:55:55 -0500

    Sesion 34 Nueva Ruta para Vista About en Home controller

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,11 +1,16 @@
 const index = (req, res) => {
-  res.render('index', { title: 'Proyecto Geminis' });
+  res.render('home/index', { title: 'Proyecto Geminis' });
 };
 
 const greeting = (req, res) => {
   res.status(200).json({ message: ' Hola Desarrollador Web' });
 };
+
+const about = (req, res) => {
+  res.render('home/about', { title: 'Acerca de ProjectNotes' });
+};
 export default {
   index,
   greeting,
+  about,
 };
